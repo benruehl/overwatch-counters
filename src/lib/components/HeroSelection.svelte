d71958bafc1c545d3135615c3a53f436b2f17f00
Benjamin Rühl <dev.ruehl@gmail.com>
2023-06-10 23:28:51 +0200

    refactor: extract hero selection component

---
+++ src/lib/components/HeroSelection.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    import type { Hero } from "../types";
+    import HeroCard from "./HeroCard.svelte";
+
+    export let heroesByRole: { [key: string]: Hero[] };
+
+    const getRoleOrder = (role: string): number => ({
+        tank: 0,
+        damage: 1,
+        support: 2
+    })[role] || -1
+
+    const roleSortFn = (a: [string, Hero[]], b: [string, Hero[]]) =>
+        getRoleOrder(a[0]) - getRoleOrder(b[0])
+</script>
+
+<section class="root">
+    {#each Object.entries(heroesByRole).sort(roleSortFn) as roleWithHeroes}
+        <div class="role-container">
+            <h3>{roleWithHeroes[0]}</h3>
+            <div class="break"></div>
+            {#each roleWithHeroes[1] as hero}
+                <HeroCard name={hero.name} imageUrl={hero.image} />
+            {/each}
+        </div>
+    {/each}
+</section>
+
+<style>
+    .root {
+        display: flex;
+        gap: 20px;
+    }
+    
+    h3 {
+        text-transform: uppercase;
+        font-family: Dosis;
+        font-weight: bold;
+        font-size: 1.2em;
+        letter-spacing: 2px;
+    }
+    
+    .role-container {
+        display: flex;
+        flex-wrap: wrap;
+        align-content: start;
+        justify-content: center;
+        gap: 4px;
+    }
+    .role-container:first-child {
+        justify-content: end;
+    }
+    .role-container:last-child {
+        justify-content: start;
+    }
+    
+    .break {
+      flex-basis: 100%;
+      height: 0;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,9 @@
 <script lang="ts">
-    import HeroCard from '../lib/components/HeroCard.svelte';
-    import type { Hero } from '../lib/types';
+    import HeroSelection from '../lib/components/HeroSelection.svelte';
     import type { PageData } from './$types';
 
     export let data: PageData;
-
-    const getRoleOrder = (role: string): number => ({
-        tank: 0,
-        damage: 1,
-        support: 2
-    })[role] || -1
-
-    const roleSortFn = (a: [string, Hero[]], b: [string, Hero[]]) =>
-        getRoleOrder(a[0]) - getRoleOrder(b[0])
 </script>
 
 <h1>Overwatch</h1>
-<section class="root">
-    {#each Object.entries(data.heroesByRole).sort(roleSortFn) as roleWithHeroes}
-        <div class="role-container">
-            <h3>{roleWithHeroes[0]}</h3>
-            <div class="break"></div>
-            {#each roleWithHeroes[1] as hero}
-                <HeroCard name={hero.name} imageUrl={hero.image} />
-            {/each}
-        </div>
-    {/each}
-</section>
-
-<style>
-.root {
-    display: flex;
-    gap: 20px;
-}
-
-h3 {
-    text-transform: uppercase;
-    font-family: Dosis;
-    font-weight: bold;
-    font-size: 1.2em;
-    letter-spacing: 2px;
-}
-
-.role-container {
-    display: flex;
-    flex-wrap: wrap;
-    align-content: start;
-    justify-content: center;
-    gap: 4px;
-}
-.role-container:first-child {
-    justify-content: end;
-}
-.role-container:last-child {
-    justify-content: start;
-}
-
-.break {
-  flex-basis: 100%;
-  height: 0;
-}
-</style>
+<HeroSelection heroesByRole={data.heroesByRole}/>
