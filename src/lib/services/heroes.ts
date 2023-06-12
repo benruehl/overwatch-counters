bb9a978c8627253f41dce766391b2be8c5acff6a
Benjamin Rühl <dev.ruehl@gmail.com>
2023-06-13 01:07:32 +0200

    fix: add base path to hero data loading

--- src/lib/services/heroes.ts
+++ src/lib/services/heroes.ts
@@ -3,7 +3,7 @@ import type { Hero } from "$lib/models";
 import { groupBy } from "$lib/utils";
 
 export const fetchHeroes = async (f: typeof fetch) => {
-    const res = await f(`/heroes.json`);
+    const res = await f(`${base}/heroes.json`);
     const heroes = await res.json() as Hero[];
 
     heroes.forEach(h => h.image = `${base}/hero-icons/${h.image}`);
